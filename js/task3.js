cd234b1e270f7fe2505e939baafda74c55b09920
Natalija_L <natalija.liske@gmail.com>
2023-06-02 11:33:14 +0300

    task3

---
+++ js/task3.js
@@ -0,0 +1,6 @@
+console.clear();
+
+const firstName = 'Eva';
+const lastName = 'Langoria';
+const str = `${firstName[2]}${firstName[1]}${firstName[0]}${lastName[7]}${lastName[6]}${lastName[5]} `;
+console.log(str);
